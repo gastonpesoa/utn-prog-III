4b1cf0a4cf369f4ba318dad6801d26223165becc
gastonpesoa <gastonpesoa@gmail.com>
2019-04-08 20:29:41 -0300

    clase3 dB

---
+++ www/Funciones/ModificarAlumno.php
@@ -0,0 +1,9 @@
+<?php
+require_once CLASES.'/alumno.php';
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$edad = $_POST['edad'];
+$dni = $_POST['dni'];
+$legajo = $_POST['legajo'];
+?>
